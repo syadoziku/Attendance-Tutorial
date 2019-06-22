babaad192c557ce9b39ce9f2dde9bd3af4f70537
syadoziku <syadoziku@icloud.com>
2019-06-22 04:18:54 +0000

    二つの小さなバグを直そうstart

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,20 @@
 class SessionsController < ApplicationController
+
   def new
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user &&user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = '認証に失敗しました。'
       render :new
     end
   end
-  
+
   def destroy
     log_out
     flash[:success] = 'ログアウトしました。'
